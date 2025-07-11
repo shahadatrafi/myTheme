29e0f8f938350c607526fa7e311efb18b7a0c77f
Shahadat Rafi <mdshahadathossenrafi@gmail.com>
2025-07-12 00:19:35 +0600

    footer widget added

--- footer.php
+++ footer.php
@@ -1,16 +1,37 @@
-    <footer>
-        <div id="copy_area">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-12">
-                        <p><?php echo get_theme_mod('rafi_footer_copy_area'); ?></p>
-                    </div>
+<?php
+
+// It is global footer template. It will visible every page and post..........
+
+?>
+
+
+
+
+
+<footer>
+
+    <div id="main_footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4"><?php dynamic_sidebar('footer-1')?></div>
+                <div class="col-md-4"><?php dynamic_sidebar('footer-2')?></div>
+                <div class="col-md-4"><?php dynamic_sidebar('footer-3')?></div>
+            </div>
+        </div>
+    </div>
+
+    <div id="copy_area">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <p><?php echo get_theme_mod('rafi_footer_copy_area'); ?></p>
                 </div>
             </div>
         </div>
-    </footer>
+    </div>
+</footer>
 
-    <?php wp_footer() ?>
+<?php wp_footer() ?>
 </body>
 
 </html>
--- inc/widgets_register.php
+++ inc/widgets_register.php
@@ -10,6 +10,33 @@ function rafi_widgets_register(){
         'before_title' => '<h3 class="widget_title">',
         'after_title' => '</h3>'
     ));
+    register_sidebar(array(
+        'name' => __('Footer Col 1', 'shahadatrafi'),
+        'id' => 'footer-1',
+        'description'=> 'You can set your footer column here',
+        'before_widget' => '<div class="child_sidebar">',
+        'after_widget'=> '</div>',
+        'before_title' => '<h3 class="widget_title">',
+        'after_title' => '</h3>'
+    ));
+    register_sidebar(array(
+        'name' => __('Footer Col 2', 'shahadatrafi'),
+        'id' => 'footer-2',
+        'description'=> 'You can set your footer column here',
+        'before_widget' => '<div class="child_sidebar">',
+        'after_widget'=> '</div>',
+        'before_title' => '<h3 class="widget_title">',
+        'after_title' => '</h3>'
+    ));
+    register_sidebar(array(
+        'name' => __('Footer Col 3', 'shahadatrafi'),
+        'id' => 'footer-3',
+        'description'=> 'You can set your footer column here',
+        'before_widget' => '<div class="child_sidebar">',
+        'after_widget'=> '</div>',
+        'before_title' => '<h3 class="widget_title">',
+        'after_title' => '</h3>'
+    ));
 }
 
 add_action('widgets_init', 'rafi_widgets_register');
