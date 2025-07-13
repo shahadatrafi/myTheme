a886f8812e3f35bda8145ca4bf6b167e35f558c9
Shahadat Rafi <mdshahadathossenrafi@gmail.com>
2025-07-13 11:33:24 +0600

    commnet secetion added for post page

--- single.php
+++ single.php
@@ -20,8 +20,10 @@ get_header();
         <div class="row">
             <div class="col-md-9">
 
-            <?php get_template_part('template_parts/post_setup') ?>
-                
+            <?php get_template_part('template_parts/post_setup'); ?>
+                <div id="comment_area">
+                    <?php comments_template(); ?>
+                </div>
             </div>
             <div class="col-md-3">
                 <?php get_sidebar(); ?>
