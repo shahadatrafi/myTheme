47040cbc00c95c195aacf4faf176a243181f7932
Shahadat Rafi <mdshahadathossenrafi@gmail.com>
2025-07-13 13:15:05 +0600

    404 page added

--- 404.php
+++ 404.php
@@ -1,11 +1,21 @@
 <?php 
 
-// Header 
+// 404 page
 get_header();
+?>
 
-
-?><div class="container d-flex justify-content-center align-items-center my-5">
-    <h2>404 page</h2>
-</div>
+<section id="body_area">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 err-page">
+                <h1 class="title_404">404 ERROR</h1>
+                <P>Opss! I think someting is wrong</P>
+                <?php get_search_form();?>
+                <a href="<?php echo home_url();?>" >Home</a>
+            </div>
+            
+        </div>
+    </div>
+</section>
 
 <?php get_footer(); ?>
