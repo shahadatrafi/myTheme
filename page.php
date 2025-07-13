cdd4e4fbd92da5a86ee63d2a5e1d9f4d38766019
Shahadat Rafi <mdshahadathossenrafi@gmail.com>
2025-07-13 11:22:08 +0600

    added single post & page templates

--- page.php
+++ page.php
@@ -1,3 +1,12 @@
+
+<?php 
+
+// This template for pages
+
+?>
+
+
+
 <?php
 
 // Header 
@@ -10,7 +19,9 @@ get_header();
     <div class="container">
         <div class="row">
             <div class="col-md-9">
-                <?php get_template_part('template_parts/blog_setup') ;?>
+
+            <?php get_template_part('template_parts/post_setup') ?>
+                
             </div>
             <div class="col-md-3">
                 <?php get_sidebar(); ?>
---
+++ template_parts/blog_setup.php
@@ -0,0 +1,29 @@
+
+<?php 
+
+// This template for blog area
+
+?>
+
+
+
+<?php
+if (have_posts()): while (have_posts()) : the_post();
+?>
+
+        <div class="blog_area">
+            <div class="post_thumb">
+                <a href="<?php the_permalink(); ?>""><?php echo the_post_thumbnail('post-thumbnails') ?></a>
+                    </div>
+                    <div class=" post_details">
+                    <h3 class="post_title"><a href="<?php the_permalink(); ?>"><?php echo the_title(); ?></a></h3>
+                    <?php echo the_excerpt(); ?>
+            </div>
+
+        </div>
+
+<?php
+    endwhile;
+else : _e('No post found');
+endif;
+?>
